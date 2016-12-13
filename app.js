e3dcb3e9592412a0b9a7d277821e3f2931544994
SunSet0418 <cds07016@naver.com>
2016-12-13 19:07:17 +0900

    ㅁㄴㅇㄹ

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ mongoose.connect("mongodb://localhost/AnAProject", function(err){
 })
 
 var ContentSchema = new schema({
+  id : {
+    type : String
+  },
   title : {
     type : String
   },
@@ -37,4 +40,46 @@ app.listen(3000, function(err){
   else{
     console.log('Server Running at 3000 Port!')
   }
+})
+
+app.post('/get', function(req, res){
+  Content.find({
+    id: req.param('id')
+  }, function(err, result){
+    if(err){
+      console.log('/get Error!')
+      throw err
+    }
+    else if(result){
+      res.json(result)
+    }
+    else{
+      console.log('Data not Founded')
+      res.json({
+        success: false,
+        message: 'Data not Founded'
+      })
+    }
+  })
+})
+
+app.post('/put', function(req, res){
+  var content = new Content({
+    id : req.param('id'),
+    title : req.param('title'),
+    content : req.param('content')
+  })
+
+  content.save(function(err){
+    if(err){
+      console.log('/put Error!')
+      throw err
+    }
+    else {
+      res.json({
+        success : true,
+        message : 'Post Success!'
+      })
+    }
+  })
 })
